c06d801e0a10c0a8fd1ea8869693bcc769a2fa8b
Kaustubh Kishor Gadakh <kaustubhgadakh2001@gmail.com>
2023-08-16 20:00:50 +0530

    Ass. Que4

---
+++ Java/Core Java/Assignment 12/Que4.java
@@ -0,0 +1,49 @@
+/*
+    4. Write a java program to do read/write operation using buffered classes
+ */
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Que4 {
+    public static void main(String[] args) {
+        //  FileOutputStream 
+        /*
+            try {
+                File file = new File("myFile.txt");
+                file.createNewFile();
+
+                FileOutputStream fin = new FileOutputStream(file);
+                BufferedOutputStream bin = new BufferedOutputStream(fin);
+                byte arr[] = {'h', 'e', 'l', 'l', 'o'};
+                fin.write(arr);
+                flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        */
+
+        //  FileInputStream
+
+        // try {
+        //         File file = new File("myFile.txt");
+        //         file.createNewFile();
+
+        //         FileInputStream fin = new FileInputStream(file);
+        //         BufferedInputStream bin = new BufferedInputStream(fin);
+        //         while (i != -1)  {
+
+        //             System.out.println((char)i);
+        //             i = fin.read();                  
+        //         }
+        //     } catch (IOException e) {
+        //         e.printStackTrace();
+        //     }
+         
+    }
+}
